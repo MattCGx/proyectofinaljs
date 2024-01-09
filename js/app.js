59c0c960e2f6235da8a3d3228a94001c97c85e31
MattCG <mattcontrerasg@gmail.com>
2024-01-09 05:50:36 -0300

    falta search y filter

--- js/app.js
+++ js/app.js
@@ -2,7 +2,8 @@ import ecomerceManager from "./src/manager/ecomerce.manager";
 import htmlElements from "./src/elements/html.elements";
 
 export const app = () => {
+
   ecomerceManager.mostrarProductos(ecomerceManager.productos);
 
-
+  
 };
