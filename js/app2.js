e5caa797e943736100dfbbf815757c129c6f58c0
MattCG <mattcontrerasg@gmail.com>
2024-01-20 02:09:45 -0300

    agregado fetch y asincronia - todo funciona

--- js/app2.js
+++ js/app2.js
@@ -1,3 +1,5 @@
+// librerias
+
 import Swal from "sweetalert2";
 import Toastify from "toastify-js";
 import "toastify-js/src/toastify.css";
@@ -7,187 +9,26 @@ let productosFilter = document.querySelector("#productosFilter");
 let buscar = document.querySelector("#buscar");
 let productosContainer = document.querySelector("#productosContainer");
 let canvasBody = document.querySelector("#canvasBody");
-let carritoPadre = document.querySelector("#carritoPadre");
 let carritoContainer = document.querySelector("#carritoContainer");
-let totalCarrito = document.querySelector("#totalCarrito");
 let botonCarrito = document.querySelector("#botonCarrito");
 let botonCarritoNotificacion = document.querySelector(
   "#botonCarritoNotificacion"
 );
-let botonComprar = document.querySelector(".botonComprar");
+
 
 // Arrays
 
 let productosFiltrados = [];
-const productosData = [
-  {
-    id: "1yey0oh",
-    nombre: "Álbum: Encantamiento Canino",
-    precio: 20.99,
-    categoria: "discografia",
-    descripcion:
-      "Sumérgete en la magia musical de Jane con su último álbum, donde cada melodía es un encantamiento para el corazón.",
-    imagen: "imagen_album1.jpeg",
-  },
-  {
-    id: "1yey0oi",
-    nombre: "Álbum: Sueños Peludos",
-    precio: 18.5,
-    categoria: "discografia",
-    descripcion:
-      "Viaja a través de los sueños peludos de Jane con este álbum lleno de ritmos contagiosos y armonías encantadoras.",
-    imagen: "imagen_album2.jpeg",
-  },
-  {
-    id: "1yey0oj",
-    nombre: "Álbum: Latidos de Pata",
-    precio: 25.75,
-    categoria: "discografia",
-    descripcion:
-      "Descubre los latidos sincronizados de las patas de Jane en cada canción de este álbum único que te hará bailar.",
-    imagen: "imagen_album3.jpeg",
-  },
-  {
-    id: "1yey0ok",
-    nombre: "Álbum: Estrellas Caninas",
-    precio: 22.0,
-    categoria: "discografia",
-    descripcion:
-      "Explora el universo de Jane a través de las estrellas caninas en este álbum que ilumina tus sentidos con cada nota.",
-    imagen: "imagen_album4.jpeg",
-  },
-
-  {
-    id: "1yey0ol",
-    nombre: "Conjunto Canino Elegante",
-    precio: 30.0,
-    categoria: "indumentaria",
-    descripcion:
-      "Viste a la moda canina con este elegante conjunto diseñado especialmente para perritas con estilo, inspirado en Jane.",
-    imagen: "imagen_prenda1.jpeg",
-  },
-  {
-    id: "1yey0om",
-    nombre: "Chaqueta Peluda de Jane",
-    precio: 22.99,
-    categoria: "indumentaria",
-    descripcion:
-      "Obtén la chaqueta peluda de Jane y siéntete tan acogedor como su abrazo. ¡Una prenda imprescindible para los amantes de Jane!",
-    imagen: "imagen_prenda2.jpeg",
-  },
-  {
-    id: "1yey0on",
-    nombre: "Vestido Estampado de Huellas",
-    precio: 35.5,
-    categoria: "indumentaria",
-    descripcion:
-      "Camina con orgullo las huellas de Jane con este encantador vestido estampado. ¡Una declaración de amor a los peludos amigos!",
-    imagen: "imagen_prenda3.jpeg",
-  },
-  {
-    id: "1yey0oo",
-    nombre: "Collar Brillante de Jane",
-    precio: 28.75,
-    categoria: "indumentaria",
-    descripcion:
-      "Luce el brillo de la estrella de Jane con este collar brillante. Una joya canina que hará que todos los ojos estén puestos en ti.",
-    imagen: "imagen_prenda4.jpeg",
-  },
-
-  {
-    id: "1yey0op",
-    nombre: "Libro: Aventuras de Jane en el Parque",
-    precio: 15.0,
-    categoria: "libros",
-    descripcion:
-      "Embárcate en las emocionantes aventuras de Jane en el parque, donde cada página cuenta una historia única llena de diversión y amistad.",
-    imagen: "imagen_libro1.jpeg",
-  },
-  {
-    id: "1yey0oq",
-    nombre: "Libro: El Secreto de la Cola Feliz",
-    precio: 18.99,
-    categoria: "libros",
-    descripcion:
-      "Descubre el secreto de la cola feliz de Jane en este encantador libro que revela las claves para una vida llena de alegría y wagging.",
-    imagen: "imagen_libro2.jpeg",
-  },
-  {
-    id: "1yey0or",
-    nombre: "Libro: Paseos Lunares con Jane",
-    precio: 21.5,
-    categoria: "libros",
-    descripcion:
-      "Únete a Jane en sus paseos lunares donde la magia se encuentra en cada rincón. Un libro que transporta a todos a un mundo de fantasía canina.",
-    imagen: "imagen_libro3.jpeg",
-  },
-  {
-    id: "1yey0os",
-    nombre: "Libro: Jane y la Búsqueda del Hueso Dorado",
-    precio: 24.75,
-    categoria: "libros",
-    descripcion:
-      "Acompaña a Jane en su épica búsqueda del hueso dorado, una historia llena de intriga, valentía y travesuras caninas.",
-    imagen: "imagen_libro4.jpeg",
-  },
-
-  {
-    id: "1yey0ot",
-    nombre: "DVD: Jane en la Gran Pantalla",
-    precio: 12.99,
-    categoria: "DVD",
-    descripcion:
-      "Disfruta de las emocionantes actuaciones de Jane en la gran pantalla con este DVD que captura la esencia mágica de sus momentos más destacados.",
-    imagen: "imagen_dvd1.jpeg",
-  },
-  {
-    id: "1yey0ou",
-    nombre: "DVD: Documental de Jane, la Exploradora",
-    precio: 14.5,
-    categoria: "DVD",
-    descripcion:
-      "Sumérgete en la vida de Jane, la exploradora canina, a través de este fascinante documental que revela sus secretos, alegrías y descubrimientos.",
-    imagen: "imagen_dvd2.jpeg",
-  },
-  {
-    id: "1yey0ov",
-    nombre: "DVD: Aventuras Nocturnas de Jane",
-    precio: 19.75,
-    categoria: "DVD",
-    descripcion:
-      "Explora las emocionantes aventuras nocturnas de Jane con este DVD lleno de misterio, diversión y momentos inolvidables bajo la luna.",
-    imagen: "imagen_dvd3.jpeg",
-  },
-  {
-    id: "1yey0ow",
-    nombre: "DVD: Jane y los Caninos del Futuro",
-    precio: 16.0,
-    categoria: "DVD",
-    descripcion:
-      "Viaja al futuro canino con Jane y su pandilla en este DVD que combina ciencia ficción, risas y lecciones de vida canina.",
-    imagen: "imagen_dvd4.jpeg",
-  },
-];
+let productosData = [];
 let carritoCompra = JSON.parse(localStorage.getItem("carrito")) || [];
 
-// Clases
-//class Producto {
-//constructor() {
-//this.id = Date.now().toString(36);
-//this.nombre = nombre;
-//this.precio = precio;
-//this.categoria = categoria;
-//this.descripcion = texto;
-//this.imagen = imagen;
-//}
-//}
+//fetch
 
-//class testimonio {
-//  constructor() {
-//    this.nombre = nombre;
-//    this.texto = texto;
-//  }
-//}
+const importarProductos = async () => {
+  const resp = await fetch("/data/productos.json");
+  productosData = await resp.json();
+  mostrarProductos(productosData);
+}
 
 // Funciones:
 
@@ -236,7 +77,7 @@ const verCarrito = (carritoCompra) => {
     carritoContainer.innerHTML = " ";
     carritoCompra.forEach((producto) => {
       let cardCarrito = document.createElement("li");
-      cardCarrito.className = "cardCanvasCarrito list-group-item";
+      cardCarrito.className = "cardCanvasCarrito list-group-item fs-4";
       cardCarrito.innerText = `${producto.nombre}`;
       let cardCarritoPrecioyEliminar = document.createElement("div");
       cardCarritoPrecioyEliminar.className = "row";
@@ -260,6 +101,27 @@ const verCarrito = (carritoCompra) => {
     <h4 class="offcanvas-title text-center">¿Por qué no revisas nuestra tienda y agregas algunos productos?</h4>
      `;
   }
+};
+
+const verTotal = (carrito) => {
+  const total = carrito
+    .reduce((acumulador, producto) => {
+      return acumulador + producto.precio;
+    }, 0)
+    .toFixed(2);
+
+  if (carrito.length > 0) {
+
+    let totalyPagar = document.createElement("div");
+    totalyPagar.className = "row";
+    totalyPagar.innerHTML = `
+    <div class="fontTitulo badge border shadow rounded text-decoration-none mt-1 fs-4">
+    Total a Pagar: $${total} USD
+    </div>
+    <button type="button" href="www.mercadopago.com" class="btn fontTitulo mt-1 shadow btn-success fs-4">Pagar</button>        
+    `;
+    carritoContainer.appendChild(totalyPagar);
+  } 
 };
 
 const agregarAlCarrito = (productoID) => {
@@ -303,6 +165,7 @@ const agregarAlCarrito = (productoID) => {
         timer: 1000,
       });
 
+      verCarrito(carritoCompra);
       verCarritoNotificacion();
       verTotal(carritoCompra);
     } else {
@@ -315,18 +178,6 @@ const agregarAlCarrito = (productoID) => {
       });
     }
   });
-};
-
-const verTotal = (carrito) => {
-  const total = carrito.reduce((acumulador, producto) => {
-    return acumulador + producto.precio;
-  }, 0).toFixed(2);
-
-  if (carrito.length > 0) {
-    totalCarrito.innerText = `Total a Pagar: $${total} USD`;
-  } else {
-    totalCarrito.innerText = " ";
-  }
 };
 
 const eliminarProductoCarrito = (productoIndex) => {
@@ -436,16 +287,14 @@ const filtrarProductos = (categoriaProducto) => {
   }
 };
 
-verCarritoNotificacion();
-mostrarProductos(productosData);
-verCarrito(carritoCompra);
+
 
 buscar.oninput = (event) => {
   if (event.target.value === " ") {
     mostrarProductos(productosData);
   } else {
     productosFiltrados = productosData.filter((producto) =>
-      producto.nombre.toLowerCase().includes(event.target.value)
+      producto.nombre.toLowerCase().includes(event.target.value.toLowerCase())
     );
     mostrarProductos(productosFiltrados);
   }
@@ -459,3 +308,10 @@ botonCarrito.onclick = () => {
   verCarrito(carritoCompra);
   verTotal(carritoCompra);
 };
+
+
+importarProductos();
+
+
+verCarritoNotificacion();
+verCarrito(carritoCompra);
